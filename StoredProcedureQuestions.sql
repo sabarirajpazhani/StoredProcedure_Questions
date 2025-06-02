96002c715ba7fd88a02ef61844d07d0351a63fe8
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-02 16:51:58 +0530

    Question 1

--- StoredProcedureQuestions.sql
+++ StoredProcedureQuestions.sql
@@ -53,5 +53,17 @@ INSERT INTO Employee (FirstName, LastName, DepartmentID, Salary, HireDate, Manag
 ('Alice', 'Johnson', 2, 65000, '2020-06-20', 1, '2023-06-01'),
 ('Bob', 'Brown', 3, 48000, '2017-11-12', NULL, '2020-03-15');
 
+select * from Employee;
 
+
+-- 1. Create a procedure to get employees with salary greater than a given amount.
+create procedure spEmpSalary
+	@Amount decimal(10,2)
+as
+begin
+	select * from Employee
+	where Salary > @Amount
+end;
+
+exec spEmpSalary 50000
 
